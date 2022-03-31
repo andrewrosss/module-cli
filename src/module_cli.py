6183fc8601f87959867dd652990b4b83bdf9b307
Andrew Ross <andrew.ross.mail@gmail.com>
2022-03-31 17:10:43 -0400

    Update module_cli.py

--- src/module_cli.py
+++ src/module_cli.py
@@ -2,11 +2,63 @@ from __future__ import annotations
 
 import argparse
 from io import TextIOWrapper
-from pathlib import Path
 from typing import NoReturn
 
 
 __version__ = "0.1.0"
+
+MODULE_S = """\
+from __future__ import annotations
+
+import argparse
+from typing import NoReturn
+
+
+__version__ = "0.1.0"
+
+
+def cli() -> NoReturn:
+    raise SystemExit(main())
+
+
+def main() -> int:
+    parser = create_parser()
+    args = parser.parse_args()
+
+    if hasattr(args, "handler"):
+        return args.handler(args)
+
+    parser.print_help()
+    return 1
+
+
+def create_parser(
+    parser: argparse.ArgumentParser | None = None,
+) -> argparse.ArgumentParser:
+    parser = parser or argparse.ArgumentParser()
+    parser.add_argument("-v", "--version", action="version", version=__version__)
+
+    # ---------------------------------
+    # CONFIGURE COMMAND-LINE ARGUGMENTS
+    # ---------------------------------
+
+    parser.set_defaults(handler=handler)
+
+    return parser
+
+
+def handler(args: argparse.Namespace) -> int:
+    # ------------------
+    # ADD BUSINESS LOGIC
+    # ------------------
+    print("Hello, World!")
+
+    return 0
+
+
+if __name__ == "__main__":
+    cli()
+"""
 
 
 def cli() -> NoReturn:
@@ -44,11 +96,7 @@ def create_parser(
 
 def handler(args: argparse.Namespace) -> int:
     out: TextIOWrapper = args.out
-
-    starter_filename = Path(__file__).parent / "_starter.py"
-    with open(starter_filename) as f:
-        starter_s = f.read()
-        out.write(starter_s)
+    out.write(MODULE_S)
 
     return 0
 
