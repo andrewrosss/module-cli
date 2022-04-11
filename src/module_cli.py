9144c84b56d29434fd6d748cd49d50398071a430
Andrew Ross <andrew.ross.mail@gmail.com>
2022-04-11 13:33:36 -0400

    Update src/module_cli.py
    - Add '--debug' flag to parser
    - handle 'Exception' in 'main'
    - have 'main' function expect command-line arguments or None

--- src/module_cli.py
+++ src/module_cli.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import argparse
 from io import TextIOWrapper
 from typing import NoReturn
+from typing import Sequence
 
 
 __version__ = "1.0.0"  # module-cli version
@@ -76,21 +77,32 @@ def cli() -> NoReturn:
     raise SystemExit(main())
 
 
-def main() -> int:
+def main(args: Sequence[str] | None = None) -> int | str:
     parser = create_parser()
-    args = parser.parse_args()
+    ns = parser.parse_args(args)
+    debug: bool = ns.debug
 
-    if hasattr(args, "handler"):
-        return args.handler(args)
-
-    parser.print_help()
-    return 1
+    try:
+        return ns.handler(ns)
+    except Exception as e:
+        if debug:
+            raise
+        else:
+            return str(e)
 
 
 def create_parser(
     parser: argparse.ArgumentParser | None = None,
 ) -> argparse.ArgumentParser:
     parser = parser or argparse.ArgumentParser()
+    parser.add_argument("-v", "--version", action="version", version=__version__)
+    parser.add_argument(
+        "-D",
+        "--debug",
+        action="store_true",
+        default=False,
+        help="run program in debug mode",
+    )
     parser.add_argument(
         "out",
         type=argparse.FileType("w"),
@@ -98,15 +110,14 @@ def create_parser(
         default="-",
         help="File to write to. (default: %(default)s)",
     )
-    parser.add_argument("-v", "--version", action="version", version=__version__)
 
     parser.set_defaults(handler=handler)
 
     return parser
 
 
-def handler(args: argparse.Namespace) -> int:
-    out: TextIOWrapper = args.out
+def handler(ns: argparse.Namespace) -> int:
+    out: TextIOWrapper = ns.out
     out.write(MODULE_S)
 
     return 0
